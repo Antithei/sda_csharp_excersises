294c88e4090163156521f1009e166539250884b9
Wiktoria <Wiktoriakoerber@interia.pl>
2022-05-14 15:43:05 +0200

    Code Cleanup

--- sda_csharp_excersises/Program.cs
+++ sda_csharp_excersises/Program.cs
@@ -8,18 +8,16 @@ namespace sda_csharp_excersises
         {
             int[] numbers = { 4, 6, 9, 14, 2, 7, 4, 8 };
             BubbleSort(numbers);
-            foreach (var number in numbers)
-            {
-                Console.WriteLine(number);
-            }
+            foreach (int number in numbers) Console.Write($"{number} ");
+
         }
 
         static void BubbleSort(int[] numbers)
         {
             int temp;
-            for(int i=numbers.Length-1; i>0;i--)
-                for(int j=0; j<i;j++)
-                    if(numbers[j]>numbers[j+1])
+            for (int i = numbers.Length - 1; i > 0; i--)
+                for (int j = 0; j < i; j++)
+                    if (numbers[j] > numbers[j + 1])
                     {
                         temp = numbers[j];
                         numbers[j] = numbers[j + 1];
