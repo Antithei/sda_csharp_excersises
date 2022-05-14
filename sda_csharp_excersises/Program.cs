220b9de66a38b4cf8badfae9adf5ff6865009506
Wiktoria <Wiktoriakoerber@interia.pl>
2022-05-14 15:38:19 +0200

    E10 solved

--- sda_csharp_excersises/Program.cs
+++ sda_csharp_excersises/Program.cs
@@ -6,7 +6,25 @@ namespace sda_csharp_excersises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution here...
+            int[] numbers = { 4, 6, 9, 14, 2, 7, 4, 8 };
+            BubbleSort(numbers);
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
+        static void BubbleSort(int[] numbers)
+        {
+            int temp;
+            for(int i=numbers.Length-1; i>0;i--)
+                for(int j=0; j<i;j++)
+                    if(numbers[j]>numbers[j+1])
+                    {
+                        temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
         }
     }
 }
