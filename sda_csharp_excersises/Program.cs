c060338a99522b25950898e4c0a22a876d8e913a
Wiktoria <Wiktoriakoerber@interia.pl>
2022-05-14 09:55:17 +0200

    E01 solved

--- sda_csharp_excersises/Program.cs
+++ sda_csharp_excersises/Program.cs
@@ -6,7 +6,26 @@ namespace sda_csharp_excersises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution here...
+            try
+            {
+                Console.WriteLine("Podaj cenę produktu: ");
+                int price = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("podaj liczbe linii: ");
+                int lines = Convert.ToInt32(Console.ReadLine());
+                PriceList(price, lines);
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void PriceList(int price, int lines)
+        {
+            for(int i=1;i<=lines;i++)
+            {
+                Console.WriteLine($"Liczba sztuk: {i} łączna cena: {i*price}zł.");
+            }
         }
     }
 }
