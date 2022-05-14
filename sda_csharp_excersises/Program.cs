754c19f99bf95a4eb69e5e4678b73ac1bfc82c43
Wiktoria <Wiktoriakoerber@interia.pl>
2022-05-14 10:21:43 +0200

    E02 solved

--- sda_csharp_excersises/Program.cs
+++ sda_csharp_excersises/Program.cs
@@ -6,7 +6,25 @@ namespace sda_csharp_excersises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution here...
+            Console.WriteLine("Podaj podstawę potęgi: ");
+            int podstawa = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj wykładnik potęgi: ");
+            int wykładnik;
+            do
+            {
+                wykładnik = Convert.ToInt32(Console.ReadLine());
+            } while (wykładnik <= 0);
+            potęga(podstawa, wykładnik);
+        }
+
+        static void potęga(int podstawa, int wykładnik)
+        {
+            int potęga = podstawa;
+            for (int i = 1; i < wykładnik; i++)
+            {
+                potęga = potęga * podstawa;
+            }
+            Console.WriteLine($"Potęga o podanych danach wynosi: {potęga}");
         }
     }
 }
