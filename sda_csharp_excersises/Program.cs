1be91738af158f5922c8956299e00ba31f3261f7
Wiktoria <Wiktoriakoerber@interia.pl>
2022-05-14 14:06:58 +0200

    E08 solved

--- sda_csharp_excersises/Program.cs
+++ sda_csharp_excersises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace sda_csharp_excersises
 {
@@ -6,7 +7,39 @@ namespace sda_csharp_excersises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution here...
+            Solution1();
+            Console.WriteLine(" ");
+            Solution2();
         }
+        static void Solution1()
+        {
+            int[] numbers = { 2, 5, 1, 2, 5 };
+
+            int[] result = numbers.Distinct().ToArray();
+            foreach (int num in result)
+            {
+                Console.Write(num);
+            }
+        }
+        static void Solution2()
+        {
+            int[] numbers = { 2, 5, 1, 2, 5 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        numbers[j] = -1;
+                    }
+                }
+                if (numbers[i] >= 0)
+                {
+                    Console.Write(numbers[i]);
+                }
+            }
+        }
+
     }
 }
